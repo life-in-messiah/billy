85076bcd03931499403dc340e8a98715c2cf1613
Joshua Austin <joshthetechie@outlook.com>
2017-01-11 00:30:00 -0600

    SMS integration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # billy
-[![Build Status](https://travis-ci.org/life-in-messiah/billy.svg?branch=master)](https://travis-ci.org/life-in-messiah/billy) [![codecov](https://codecov.io/gh/life-in-messiah/billy/branch/master/graph/badge.svg)](https://codecov.io/gh/life-in-messiah/billy) ![Python versions](https://img.shields.io/badge/Python-2.7%2C%203.4%2C%203.5%2C%20pypy-blue.svg)  
-Slackbot for integrating with realtime messaging platforms
+[![Build Status](https://travis-ci.org/life-in-messiah/billy.svg?branch=master)](https://travis-ci.org/life-in-messiah/billy) ![Python versions](https://img.shields.io/badge/Python-2.7%2C%203.4%2C%203.5%2C%203.6%2C%20pypy-blue.svg)  
+Slackbot for integrating with realtime messaging platforms  
+### PHASE I: Headless SMS integration
+I was able to have the bot place incoming SMS mesages into number-specific channels and then have users send their responses from those channels with the `/send` command.
--- billybot.py
+++ billybot.py
@@ -3,15 +3,85 @@
 Billy - A Slackbot for integrating with realtime messaging platforms
 """
 import os
+from flask import Flask, request, Response
 from slackclient import SlackClient
+from twilio import twiml
+from twilio.rest import TwilioRestClient
 
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", None)
+SLACK_WEBHOOK_SECRET = os.environ.get("SLACK_WEBHOOK_SECRET", None)
+TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER", None)
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID", None)
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN", None)
 
-class Billy:
+BOT = SlackClient(SLACK_BOT_TOKEN)
+SMS = TwilioRestClient()
+app = Flask(__name__)
 
-    def __init__(self, token=None):
-        self.client = SlackClient(os.environ.get('SLACK_BOT_TOKEN')) if not token else token
+def known_channels():
+    response = BOT.api_call("channels.list")
+    for channel in response['channels']:
+        yield "#" + channel['name']
 
-    def send_slack_message(self, channel, text):
-        res = self.client.api_call("chat.postMessage", channel=channel, text=text, as_user=True)
-        if not res['ok']:
-            raise AttributeError("API call did not return OK: " + str(res))
+def archived_channels():
+    pass #TODO
+
+def safe_call(action, **kwargs):
+    res = BOT.api_call(action, username="billy", icon_emoji=":iphone:", **kwargs)
+    if not res['ok']:
+        raise Exception("API call did not return OK: " + str(res))
+
+def _log(message):
+    safe_call("chat.postMessage",
+              channel="#search-dev",
+              text=message)
+
+@app.route("/twilio", methods=["POST"])
+def receive_message():
+    response = twiml.Response()
+    message = request.form["Body"]
+    channel_name = "#sms_" + request.form["From"].replace("+", "")
+    if channel_name not in known_channels():
+        try:
+            safe_call("channels.create", name=channel_name)
+            #TODO: invite anybody active who is not a robot
+            safe_call("chat.postMessage",
+                      channel=channel_name,
+                      text="Received: "+message)
+            safe_call("chat.postMessage",
+                      channel="#search-chats",
+                      text="New message at: " + channel_name + \
+                           "\nMessage: " + message,
+                      link_names=1)
+        except Exception as e:
+            _log("Error while creating channel:\n" + str(e))
+    else:
+        #TODO: if the channel is archived, unarchive it and invite anybody active who is not a robot
+        safe_call("chat.postMessage",
+                  channel=channel_name,
+                  text="Received: "+message)
+    return Response(response.toxml(), mimetype="text/xml"), 200
+
+@app.route("/slack", methods=["POST"])
+def send_message():
+    #if request.form["token"] == SLACK_WEBHOOK_SECRET:
+        channel = request.form['channel_name']
+        text = request.form['text'].replace("@billy ", "")
+        if channel.startswith("sms_"):
+            to_number = "+" + channel.replace("sms_", "")
+            _log("Sending text to " + to_number)
+            SMS.messages.create(to=to_number, from_=TWILIO_NUMBER, body=text)
+            safe_call("chat.postMessage",
+                      channel="#"+channel,
+                      text="Sent: "+text)
+        else:
+            safe_call("chat.postMessage",
+                      channel=channel,
+                      text="Not a valid channel to send text messages from")
+        return Response(), 200
+    #_log("Encountered a 403")
+    #return Response(), 403
+
+if __name__ == "__main__":
+    app.config["DEBUG"] = True
+    app.run()
